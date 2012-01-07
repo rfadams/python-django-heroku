2aba3e4871663b1e3fbe9df369a7b03eaa42f2a9
Robert Adams <rfadams@gmail.com>
2012-01-07 13:07:56 -0800

    Init.

---
+++ lib/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home/homepage.html'}),
+    (r'^login$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
+    (r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/login'}),
+
+    (r'^accounts/profile/$', 'django.views.generic.simple.direct_to_template', {'template': 'accounts/profile.html'}),
+
+    # Examples:
+    # url(r'^$', 'lib.views.home', name='home'),
+    # url(r'^lib/', include('lib.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^adminsite/', include(admin.site.urls)),
+)

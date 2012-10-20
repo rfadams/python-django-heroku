110168926cc23ca6673ed4e9446ded32c6233a51
Robert Adams <rfadams@gmail.com>
2012-10-19 20:06:00 -0700

    Modified login form to use Class-based view to allow for greater flexibility.

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.base import TemplateView
 
-from accounts.views import CreateUserView, EditUserView
+from accounts.views import CreateUserView, EditUserView, LoginUserView
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'generic/form.html'}, name='login'),
+    url(r'^login/$', LoginUserView.as_view(), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}, name='logout'),
 
     url(r'^signup/$', CreateUserView.as_view(), name='signup'),

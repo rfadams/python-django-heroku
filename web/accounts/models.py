f67aff7a31b20512b4b94d212af48c6103d7c4b9
Robert Adams <rfadams@gmail.com>
2013-06-15 18:15:30 -0700

    BugFixed UserProfile post_save hook to handle the initial syncdb w/o error.

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, utils
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
@@ -12,7 +12,10 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         hash = rand_str(size=8)
-        UserProfile.objects.create(user=instance, hash=hash)
+        try:
+            UserProfile.objects.create(user=instance, hash=hash)
+        except utils.DatabaseError:
+            pass
 
 post_save.connect(create_user_profile, sender=User)
 

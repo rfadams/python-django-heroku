328e33710dc83e2df513d5f90feaaadcf0e91d9f
Robert Adams <rfadams@gmail.com>
2015-11-16 14:25:12 -0800

    disabled accounts app

--- web/settings.py
+++ web/settings.py
@@ -222,7 +222,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'accounts',
+    # 'accounts',
 
 	'south',
     'django_extensions',
